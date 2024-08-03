4928f2ed2baa9e65454b34b7220cdc6a80e6622d
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-08-03 13:55:12 +0900

    feat: implement Gemini API sample test

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,19 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {onRequest} = require("firebase-functions/v2/https");
+const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Access your API key as an environment variable (see "Set up your API key" above)
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-exports.helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+exports.sayHello = onRequest({ cors: true }, async (req, res) => {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const prompt =
+    "만약 내일 세상이 멸망한다면 누구랑, 어디에서, 무엇을 하고 말해주면 해당 내용을 한 줄로 요약해주고, 어울리는 철학자나 유명인의 명언 하나를 알려줘. 나는 '좋아하는 아이돌'이랑 '콘서트장'에서 '하루종일 노래를 듣고 싶어'";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log('ai결과', response)
+  res.status(200).send(text);
 });

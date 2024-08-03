8ea4fd93f367fef77b939432ebaeb5095f281c9e
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-08-03 19:34:04 +0900

    feat: implement Gemini Model AI with display Quaote result

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,64 @@
 const { onRequest } = require("firebase-functions/v2/https");
 const logger = require("firebase-functions/logger");
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const {
+  GoogleGenerativeAI,
+  HarmCategory,
+  HarmBlockThreshold,
+} = require("@google/generative-ai");
 
-// Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 
-exports.sayHello = onRequest({ cors: true }, async (req, res) => {
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+exports.generateSummaryAndQuote = onRequest(
+  { cors: true },
+  async (req, res) => {
+    try {
+      const { who, where, what } = req.body;
 
-  const prompt =
-    "만약 내일 세상이 멸망한다면 누구랑, 어디에서, 무엇을 하고 말해주면 해당 내용을 한 줄로 요약해주고, 어울리는 철학자나 유명인의 명언 하나를 알려줘. 나는 '좋아하는 아이돌'이랑 '콘서트장'에서 '하루종일 노래를 듣고 싶어'";
+      if (!who || !where || !what) {
+        res
+          .status(400)
+          .send("Invalid request: 'who', 'where', and 'what' are required.");
+        return;
+      }
 
-  const result = await model.generateContent(prompt);
-  const response = await result.response;
-  const text = response.text();
-  console.log('ai결과', response)
-  res.status(200).send(text);
-});
+      const model = genAI.getGenerativeModel({
+        model: "gemini-1.5-flash",
+        safetySetting: [
+          {
+            category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
+          },
+          {
+            category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
+          },
+          {
+            category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
+          },
+          {
+            category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
+          },
+          {
+            category: HarmCategory.HARM_CATEGORY_UNSPECIFIED,
+            threshold: HarmBlockThreshold.BLOCK_NONE,
+          },
+        ],
+        generationConfig: { responseMimeType: "application/json" },
+      });
+
+      const prompt = `만약 내일 세상이 멸망한다면 ${who}랑 ${where}에서, ${what}을 하고 싶어요. 해당 내용을 한 줄로 '당신은 지금, 내일 세상이 멸망한다면,'를 시작으로 요약해주고, 어울리는 철학자나 유명인의 명언 하나를  '이름, 명언' 형식으로 알려주고 간단하게 설명해줘. 결과값은 json형식으로 . 각 변수를 summary, quote, info로 설정해줘`;
+
+      const result = await model.generateContent(prompt);
+      const response = await result.response.text();
+      const parsedResponse = JSON.parse(response);
+      console.log("ai결과", parsedResponse);
+
+      res.status(200).send(parsedResponse);
+    } catch (error) {
+      logger.error("Error generating content", error);
+      res.status(500).send("Error generating content");
+    }
+  }
+);
--- src/app/quotes/page.tsx
+++ src/app/quotes/page.tsx
@@ -1,56 +1,79 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
+import axios from "axios";
 
-const Main: React.FC = () => {
-  const [isClient, setIsClient] = useState<boolean>(false);
+const Results: React.FC = () => {
+  const [result, setResult] = useState<any>(null);
   const router = useRouter();
 
-  let storedWho = localStorage.getItem("quotes-who");
-  let storedWhere = localStorage.getItem("quotes-where");
-  let storedWhat = localStorage.getItem("quotes-what");
+  useEffect(() => {
+    const fetchResults = async () => {
+      const who = localStorage.getItem("quotes-who");
+      const where = localStorage.getItem("quotes-where");
+      const what = localStorage.getItem("quotes-what");
 
-  const handleSubmit = async () => {
-    localStorage.setItem("quotes-who", "");
-    localStorage.setItem("quotes-where", "");
-    localStorage.setItem("quotes-what", "");
+      if (!who || !where || !what) {
+        router.push("/");
+        return;
+      }
+
+      try {
+        const response = await axios.post(
+          "http://127.0.0.1:5001/ima-quotes-project/us-central1/generateSummaryAndQuote",
+          {
+            who,
+            where,
+            what,
+          }
+        );
+        setResult(response.data);
+      } catch (error) {
+        console.error("Error fetching results:", error);
+      }
+    };
+
+    fetchResults();
+  }, [router]);
+
+  console.log("FE에서 받은 값", result);
+
+  const handleBackToMain = () => {
+    localStorage.removeItem("quotes-who");
+    localStorage.removeItem("quotes-where");
+    localStorage.removeItem("quotes-what");
     router.push("/");
   };
 
   return (
-    <>
-      <section className="w-full flex min-h-screen flex-col lg:px-28 xs:gap-10 gap-12 tb:gap-16 lg:gap-20 items-center 2xs:pt-20 xs:pt-24 2sm:pt-28 sm:pt-28 tb:pt-32 pt-40 ">
-        <div className="w-full">
-          <div className="w-full h-40 pl-8 mb-2 bg-orange-300">
-            <h3 className="font-bold 2xs:text-sm xs:text-lg text-xl">
-              당신에게 어울리는
-            </h3>
-            명언과 장면을 알려드릴게요
-          </div>
-        </div>
-        <div className="w-full flex flex-col gap-5 px-10">
-          <h3 className="text-lg font-bold">
-            만약에 지금, 내일 세상이 끝난다면
+    <section className="w-full flex min-h-screen flex-col lg:px-28 xs:gap-10 gap-12 tb:gap-16 lg:gap-20 items-center 2xs:pt-20 xs:pt-24 2sm:pt-28 sm:pt-28 tb:pt-32 pt-40 ">
+      <div className="w-full">
+        <div className="w-full h-40 pl-8 mb-2 bg-orange-300">
+          <h3 className="font-bold 2xs:text-sm xs:text-lg text-xl">
+            당신에게 어울리는 결과
           </h3>
-          <div>
-            당신은 누구 {storedWho}랑 어디 {storedWhere}에서 무엇{storedWhat}을
-            하고 싶으시네요. (AI)로 변환?
-          </div>
-          <div>
-            <h3>Gemini API 결과 값</h3>
-            <p>어울리는 사진</p>
-            <p>어울리는 명언</p>
-          </div>
-          <div>
-            <p>sns 공유 버튼</p>
-            <button onClick={handleSubmit}>
-              <p>메인으로 이동 버튼</p>
-            </button>
-          </div>
         </div>
-      </section>
-    </>
+      </div>
+      <div className="w-full flex flex-col gap-5 px-10">
+        {result ? (
+          <>
+            <h3 className="text-lg font-bold">결과</h3>
+            <p>요약: {result.summary}</p>
+            <p>명언: {result.quote}</p>
+            <p>내용: {result.info}</p>
+            <button
+              onClick={handleBackToMain}
+              className="flex w-28 h-10 bg-slate-700 rounded-lg text-white text-base font-medium text-center items-center justify-center"
+            >
+              메인으로 이동
+            </button>
+          </>
+        ) : (
+          <p>결과를 불러오는 중...</p>
+        )}
+      </div>
+    </section>
   );
 };
 
-export default Main;
+export default Results;

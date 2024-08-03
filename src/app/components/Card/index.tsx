12d4e696d8752478dfb9f955afa025b2fb13fa5d
김현조 <hyunjoebrother@gimhyeonjoui-noteubug.local>
2024-08-03 12:33:56 +0900

    feat: implement display Pocketbase data Card UI

---
+++ src/app/components/Card/index.tsx
@@ -0,0 +1,57 @@
+"use client";
+import React from "react";
+
+type CardProps = {
+  id: string;
+  who: string;
+  where: string;
+  what: string;
+  order: number;
+  created: string;
+};
+
+function formatDateToKorean(dateString: string): string {
+  const date = new Date(dateString);
+
+  const year = date.getFullYear().toString().slice(2);
+  const month = date.getMonth() + 1;
+  const day = date.getDate();
+  const hours = date.getHours();
+  // console.log(date, day, hours, '한국시간 오전오후 확인필요!')
+
+  const period = hours < 12 ? "오전" : "오후";
+  const hour12 = hours % 12 || 12;
+
+  return `${year}년 ${month}월 ${day}일 ${period} ${hour12}시쯤`;
+}
+
+const Card: React.FC<CardProps> = ({
+  id,
+  who,
+  where,
+  what,
+  order,
+  created,
+}) => {
+  return (
+    <div
+      key={id}
+      className="flex flex-col w-full h-full justify-between tb:px-3 lg:px-3 px-4 py-3 bg-[#615698] border rounded-xl shadow-md"
+    >
+      <div>
+        <h2 className="font-bold sm:text-base text-lg text-white">
+          {who}-{where}
+        </h2>
+        <p className="sm:text-sm text-sm text-white">{what}</p>
+      </div>
+      <div className="2xs:mt-6 xs:mt-6 2sm:mt-6 sm:mt-5 mt-3 lg:mt-4 flex-end flex-col items-end">
+        <p className="text-xs font-light text-gray-400">
+          {formatDateToKorean(created)}
+        </p>
+        <p className="text-sm font-bold text-gray-200">{order}번째 명언</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,56 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import PocketBase from "pocketbase";
+import Card from "./components/Card";
+
+const pb = new PocketBase("https://ima-quotes.pockethost.io");
+
+type Answer = {
+  id: string;
+  who: string;
+  where: string;
+  what: string;
+  order: number;
+  created: string;
+};
 
 const Main: React.FC = () => {
-  const [isClient, setIsClient] = useState<boolean>(false);
+  const [answersData, setAnswersData] = useState<Answer[]>([]);
+  const [isFetching, setIsFetching] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const answers = await pb
+          .collection("answers")
+          .getList(1, 30, { sort: "created" });
+        const formattedAnswers = answers.items.map((item, index) => ({
+          id: item.id,
+          who: item.who,
+          where: item.where,
+          what: item.what,
+          order: index + 1, // 순서 추가
+          created: item.created,
+        }));
+        setAnswersData(formattedAnswers);
+      } catch (error) {
+        console.error("Error fetching menus:", error);
+      } finally {
+        setIsFetching(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (isFetching) {
+    return (
+      <main className="w-full h-screen bg-white flex flex-col items-center justify-center">
+        {/* <img src={movingBus.src} alt="" className="w-[40vw] h-[40vh]" /> */}
+        <p className="text-black text-lg font-bold">로딩중...</p>
+      </main>
+    );
+  }
 
   return (
     <>
@@ -18,6 +66,19 @@ const Main: React.FC = () => {
                 나는 어떨까?
               </div>
             </a>
+          </div>
+          <div className="w-full 2xs:py-5 xs:py-5 2sm:py-6 sm:py-6 tb:py-8 lg:py-10 grid grid-cols-1 sm:grid-cols-2 tb:grid-cols-3 lg:grid-cols-4 2xs:gap-6 xs:gap-6 2sm:gap-6 gap-4">
+            {answersData.map((answer) => (
+              <Card
+                key={answer.id}
+                id={answer.id}
+                who={answer.who}
+                where={answer.where}
+                what={answer.what}
+                order={answer.order}
+                created={answer.created}
+              />
+            ))}
           </div>
         </div>
       </section>
